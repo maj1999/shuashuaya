78ee3e55be7ac1574e5144c9290e017c00498ec2
Mark <1946027533@qq.com>
2026-04-11 14:03:47 +0800

    chore: bump version to 1.2.4

--- shared/src/commonMain/kotlin/com/cleanpic/AppInfo.kt
+++ shared/src/commonMain/kotlin/com/cleanpic/AppInfo.kt
@@ -2,6 +2,6 @@ package com.cleanpic
 
 object AppInfo {
     const val APP_NAME = "刷刷鸭"
-    const val VERSION = "1.2.3"
+    const val VERSION = "1.2.4"
     val displayVersion: String get() = "$APP_NAME v$VERSION"
 }

bb040485aca0943184a5f36cef229bebfc6fa6c5
Mark <1946027533@qq.com>
2026-04-19 10:19:59 +0800

    chore: bump version to 1.2.7

--- buildSrc/src/main/kotlin/CleanPicBuildConfig.kt
+++ buildSrc/src/main/kotlin/CleanPicBuildConfig.kt
@@ -51,6 +51,6 @@ object AppConfig {
     const val APPLICATION_ID = "com.cleanpic.android"
     const val GROUP = "com.cleanpic"
     const val VERSION_NAME = "1.2.7"
-    const val VERSION_CODE = 23
+    const val VERSION_CODE = 24
     const val UPDATE_API_URL = "https://cleanpic-update.maj1946027533.workers.dev"
 }

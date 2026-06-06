63a2f115666effa5aa329bebf20d8625d6f5d507
Mark <1946027533@qq.com>
2026-06-06 10:09:46 +0800

    chore: bump version to 1.7.1

--- shared/src/commonMain/kotlin/com/cleanpic/AppInfo.kt
+++ shared/src/commonMain/kotlin/com/cleanpic/AppInfo.kt
@@ -2,8 +2,8 @@ package com.cleanpic
 
 object AppInfo {
     const val APP_NAME = "刷刷鸭"
-    const val VERSION = "1.7.0"
+    const val VERSION = "1.7.1"
     // 与 buildSrc/CleanPicBuildConfig.kt 的 VERSION_CODE 保持一致；发布脚本同步更新
-    const val VERSION_CODE = 31
+    const val VERSION_CODE = 32
     val displayVersion: String get() = "$APP_NAME v$VERSION"
 }
